a632bcea049864374b5aa457b6e2232b1c7948a3
SantoshDharmik <Santoshdharmik5@gmail.com>
2025-09-17 12:53:35 +0530

    Learn import as

---
+++ 7.Node-js/Server1/newas.js
@@ -0,0 +1,9 @@
+export function sayHello(name){
+    return `Hello, ${name}!`;
+}
+
+export function sayBye(name){
+    return `Hello, ${name}!`;
+}
+
+
--- 7.Node-js/Server1/server.js
+++ 7.Node-js/Server1/server.js
@@ -6,7 +6,12 @@ import number1 from "./script1.js"
 
 import {data1, data2, doSomthing, doSomthing2} from "./script3.js"
 
+import {sayHello as hi, sayBye as bye} from "./newas.js"
+
+import * as newas from './newas.js'
+
 let port = 4001
+
 
 let app = express()
 console.log(number)
@@ -15,6 +20,13 @@ doSomthing()
 console.log(data1)
 console.log(data2)
 doSomthing2()
+// import as /
+console.log(hi("santosh"));
+console.log(bye("santosh"));
+
+console.log(newas.sayHello("Dharmik"));
+console.log(newas.sayBye("Dharmik"));
+
 
 app.get("/",(request,response) => {
     response.send("Got this message from backend !")
